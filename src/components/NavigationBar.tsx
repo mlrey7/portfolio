2d52692330b4e9d68143b451b24c40ec1058a92a
Matthew Rey <matthewrey@zayls.com>
2024-03-28 12:14:47 +0800

    Make name a link

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import NavigationBarContent from "./NavigationBarContent";
+import Link from "next/link";
 
 const NavigationBar = () => {
   return (
     <div className="fixed flex justify-between sm:justify-center top-0 inset-x-0 bg-blue-500 py-2 z-20 items-center max-sm:px-8">
-      <h1 className="text-lg text-white sm:hidden">Matthew Rey</h1>
+      <Link href={"/"} className="text-lg text-white sm:hidden">
+        Matthew Rey
+      </Link>
+
       <NavigationBarContent />
     </div>
   );
