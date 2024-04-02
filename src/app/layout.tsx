c50659258c655baf1c35df9e4a275a31a9675ff0
Matthew Rey <matthewrey@zayls.com>
2024-04-02 08:22:14 +0800

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "@/components/NavigationBar";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID ?? ""} />
     </html>
   );
 }
