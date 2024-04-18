cfcac8895057a9e3eb707a1f4873954e91358d3b
Matthew Rey <matthewlemuelr@gmail.com>
2024-04-18 11:52:58 +0800

    Modify meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,20 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Matthew Rey",
-  description: "Matthew Rey",
+  description: "Matthew Rey's Personal Site",
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
