79b0c78dfc52e2fda3d26b79b1656aaaf90519bf
Matthew Rey <matthewlemuelr@gmail.com>
2024-04-18 11:43:13 +0800

    Add robots.txt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import AnimatedHeader from "@/components/AnimatedHeader";
 import { Icons } from "@/components/Icons";
 import ProjectGrid from "@/components/ProjectGrid";
 import Timeline from "@/components/Timeline";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,8 +11,8 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen container flex-col py-24 md:pt-48 md:px-36 gap-12 sm:gap-24">
-      <section>
+    <main className="flex min-h-screen flex-col pt-24 md:pt-48">
+      <section className="pb-6 sm:pb-12 container md:px-36">
         <div className="flex flex-col-reverse sm:flex-row w-full sm:justify-between items-center gap-4 sm:gap-0">
           <h1 className="text-4xl lg:text-5xl font-semibold leading-tight tracking-tight text-center sm:text-start">
             Hello there, I{"'"}m Matthew Lemuel Rey. <br />
@@ -83,55 +82,58 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-      <section className="flex flex-col gap-4 sm:gap-8">
-        <h2 className="text-3xl font-semibold">My Skills</h2>
-        <div className="flex flex-wrap max-w-4xl gap-4">
-          <div className="flex gap-2 items-center">
-            <Icons.react className="w-12 h-12" />
-            <h6 className="font-bold">React</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.nextjs className="w-12 h-12" />
-            <h6 className="font-bold">Next.js</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.tailwind className="w-12 h-12" />
-            <h6 className="font-bold">Tailwind CSS</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.javascript className="w-12 h-12" />
-            <h6 className="font-bold">Javascript ES6</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.typescript className="w-12 h-12" />
-            <h6 className="font-bold">Typescript</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.firebase className="w-12 h-12" />
-            <h6 className="font-bold">Firebase</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.git className="w-12 h-12" />
-            <h6 className="font-bold">Git</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.prisma className="w-12 h-12" />
-            <h6 className="font-bold">Prisma</h6>
-          </div>
-          <div className="flex gap-2 items-center">
-            <Icons.postgres className="w-12 h-12" />
-            <h6 className="font-bold">PostgreSQL</h6>
+      <section className="bg-slate-100 py-6 sm:py-12">
+        <div className="flex flex-col gap-4 sm:gap-8 container md:px-36">
+          <h2 className="text-3xl font-semibold">My Skills</h2>
+          <div className="flex flex-wrap max-w-4xl gap-4">
+            <div className="flex gap-2 items-center">
+              <Icons.react className="w-12 h-12" />
+              <h6 className="font-bold">React</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.nextjs className="w-12 h-12" />
+              <h6 className="font-bold">Next.js</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.tailwind className="w-12 h-12" />
+              <h6 className="font-bold">Tailwind CSS</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.javascript className="w-12 h-12" />
+              <h6 className="font-bold">Javascript ES6</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.typescript className="w-12 h-12" />
+              <h6 className="font-bold">Typescript</h6>
+            </div>
+            <div className="flex gap-2 items-center -ml-1">
+              <Icons.firebase className="w-12 h-12" />
+              <h6 className="font-bold">Firebase</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.git className="w-12 h-12" />
+              <h6 className="font-bold">Git</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.prisma className="w-12 h-12" />
+              <h6 className="font-bold">Prisma</h6>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Icons.postgres className="w-12 h-12" />
+              <h6 className="font-bold">PostgreSQL</h6>
+            </div>
           </div>
         </div>
       </section>
-      <section>
+      <section className="py-6 sm:py-12 container md:px-36">
         <h2 className="text-3xl font-semibold mb-4 sm:mb-8">My Projects</h2>
         <ProjectGrid />
       </section>
-      <section>
-        <h2 className="text-3xl font-semibold mb-4 sm:mb-8">My Experience</h2>
-        <Timeline />
+      <section className="bg-slate-100 py-6 pb-12 sm:py-12 sm:pb-24">
+        <div className="container md:px-36">
+          <h2 className="text-3xl font-semibold mb-4 sm:mb-8">My Experience</h2>
+          <Timeline />
+        </div>
       </section>
     </main>
   );
